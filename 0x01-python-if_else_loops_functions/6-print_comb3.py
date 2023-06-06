337386326ba1f5dcca314e87db98d817899f6e66
Domodan <ongomdaniel9@gmail.com>
2023-06-06 03:12:53 -0700

    modified and fixed issues - combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(10):
         if i == 8 and j == 9:
             print(f"{i:d}{j:d}")
         elif i != j and j > i:
-            print(f"{i:d}{j:d}, ", end="")
+            print("{:d}{:d}, ".format(i, j), end="")
