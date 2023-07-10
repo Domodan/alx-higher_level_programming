d232b7929a694fb0b5e27eafc8e6da2d77abeb2c
Domodan <ongomdaniel9@gmail.com>
2023-07-10 12:47:46 -0700

    improved Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+Module: BaseGeometry
+
+"""
+
+
+class BaseGeometry:
+    """
+
+    Class: BaseGeometry
+
+    Method: area() - Raises an Exception
+
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
