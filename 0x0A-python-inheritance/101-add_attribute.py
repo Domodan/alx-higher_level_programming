f93fac9655b11973133a389d6dad8527ec6e2908
Domodan <ongomdaniel9@gmail.com>
2023-07-10 13:23:25 -0700

    can I add attributes?

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Module: Contain a single function
+
+"""
+
+
+def add_attribute(self, attribute, value):
+    """
+
+    Add attribute if it's possible
+
+    """
+    if hasattr(self, '__dict__'):
+        setattr(self, attribute, value)
+    else:
+        raise TypeError("can't add new attribute")
