de983518776f3b7c456bca382e3964a0bc8ed920
Domodan <ongomdaniel9@gmail.com>
2023-07-11 10:41:50 -0700

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """
-   Module: adds all arguments to a Python List and save to file
+    Module: Handles file input and saves to file
 """
 
-import sys
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+from sys import argv
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-if __name__ == "__main__":
-
-    filename = "add_item.json"
-
-    try:
-        arg_list = load_from_json_file(filename)
-    except:
-        arg_list = []
-
-    for arg in sys.argv[1:]:
-        arg_list.append(arg)
-    save_to_json_file(arg_list, filename)
+argv.pop(0)
+try:
+    deserialized = load_from_json_file("add_item.json")
+    if deserialized is None:
+        save_to_json_file(argv, "add_item.json")
+    else:
+        deserialized.extend(argv)
+        save_to_json_file(deserialized, "add_item.json")
+except FileNotFoundError:
+    save_to_json_file(argv, "add_item.json")
