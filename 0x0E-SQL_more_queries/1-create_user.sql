43ce9ab2c6a7e37dd6cc02786509b49fe791669c
Domodan <ongomdaniel9@gmail.com>
2023-08-16 23:10:14 -0700

    create and grant privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,5 @@
 -- Creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-
-GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost'
-WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
