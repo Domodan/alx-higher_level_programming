cf0cd304e15d10946b5d1c62d3c3d893d3d9cc17
Domodan <ongomdaniel9@gmail.com>
2023-08-16 23:23:54 -0700

    create and grant privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,10 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Grants for user_0d_1@localhost
+SELECT
+    CONCAT('GRANT ', GROUP_CONCAT(privilege_type), ' ON *.* TO `user_0d_1`@`localhost`') AS grants
+FROM
+    information_schema.user_privileges
+WHERE
+    grantee = 'user_0d_1@localhost'
+GROUP BY
+    grantee;
+
