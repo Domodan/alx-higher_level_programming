2ad4a2f6044463ba8adf2a1cadb1411936bb5b39
Domodan <ongomdaniel9@gmail.com>
2023-09-29 04:37:40 -0700

    Find a peak - Technical interview

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,14 +12,16 @@ def find_peak(list_of_integers):
         Return:
             int(the peak integer(s))
     """
-    if len(list_of_integers) == 0:
+    if list_of_integers == []:
         return None
-    if len(list_of_integers) == 1:
+
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    if len(list_of_integers) == 2:
+    elif size == 2:
         return max(list_of_integers)
 
-    mid = int(len(list_of_integers) / 2)
+    mid = int(size / 2)
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
