464d460d92d919797e8f95969356c3a12131c137
domodan <ongomdaniel9@gmail.com>
2023-10-24 02:04:15 -0700

    a script that gets the contents of a webpage and stores it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const fileName = process.argv[3];
+
+request(url, (error, response, body) => {
+  const data = body;
+  fs.writeFile(fileName, data, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
